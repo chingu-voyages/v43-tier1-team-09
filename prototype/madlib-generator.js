c1428b47ad1692dcbf239b1ac18374ad7a5f59c9
Mikey Nichols <33106391+mnichols08@users.noreply.github.com>
2023-03-09 06:45:52 -0500

    created a console.log version of the app for fun

---
+++ prototype/madlib-generator.js
@@ -0,0 +1,41 @@
+const peachesGen = (place,noun,secondNoun,ryhmesWithSecond,building,adjective,appendage,typeOfFood) => 
+`Movin' to the ${place}
+Gonna eat a lot of ${noun}es
+Movin' to the ${place}
+Gonna eat me a lot of ${noun}es
+Movin' to the ${place}
+Gonna eat a lot of ${noun}es
+Movin' to the ${place}
+I'm gonna eat a lot of ${noun}es
+${noun}es come from a ${secondNoun}
+They were put there by a ${ryhmesWithSecond}
+In a ${building} downtown
+If I had my little way
+I'd eat ${noun}es every day
+${adjective} bulges in the shade
+Movin' to the ${place}
+Gonna eat a lot of ${noun}es
+Movin' to the ${place}
+Gonna eat me a lot of ${noun}es
+Movin' to the ${place}
+Gonna eat a lot of ${noun}es
+Movin' to the ${place}
+Gonna eat a lot of ${noun}es
+Took a little nap where the roots all twist
+Squished a rotten ${noun} in my fist
+And dreamed about you, woman
+I poked my ${appendage} down inside
+Make a little room for an <type of bug> to hide
+Nature's ${secondNoun} in my hand or ${secondNoun} or a ${typeOfFood}
+Millions of ${noun}es, ${noun}es for me
+Millions of ${noun}es, ${noun}es for free
+Millions of ${noun}es, ${noun}es for me
+Millions of ${noun}es, ${noun}es for free
+Look out
+Millions of ${noun}es, ${noun}es for me
+Millions of ${noun}es, ${noun}es for free
+Millions of ${noun}es, ${noun}es for me
+Millions of ${noun}es, ${noun}es for free
+Look out`
+//console.log(peachesGen('country','peach','can','man','factory','sun-soakin','finger','pie'))
+console.log(peachesGen('Silicon Valley','bit','bit','a bunch more bits','cpu core','energetic','thought','pizza'))
