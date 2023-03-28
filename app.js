67b7045d84cb89f0571ac1aaf0861d84a3d5114b
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-03-28 16:14:07 -0300

    Declare a variable for the anchor container

---
+++ app.js
@@ -0,0 +1,4 @@
+const anchor =
+  document.querySelector(
+    "main"
+  ); /* as recommend, anchored everything to the <main>, so if we declare the variable it no longer matters it is just defined */
