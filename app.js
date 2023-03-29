95a17480c06f7ffdd4938de4b3f57650806089f3
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-03-29 12:31:27 -0300

    create a function to collect Scanarios

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const anchor =
   document.querySelector(
     "main"
   ); /* as recommend, anchored everything to the <main>, so if we declare the variable it no longer matters it is just defined */
+
+function collectScenarios() {
+  const json = fetch("./stories.json").then((response) =>
+    response.json().then((data) => data.map((data) => data))
+  );
+  return json;
+}
