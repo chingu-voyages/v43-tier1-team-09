0945a5ca602b3c58abd06a4bd553b49f25bb3794
Emimint <90863470+Emimint@users.noreply.github.com>
2023-03-30 14:07:24 -0400

    search all words within scenarios

--- formatter.py
+++ formatter.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 path_to_fileA = "scenarios.json"
 
@@ -11,11 +12,15 @@ data = json.load(f)
 print(data)
 # exit(0)
 
+# print scenarios and words found:
+for key in data:
+    delimiter_strs = key['Delimiter']
+    my_regex = "\\" + delimiter_strs[0] + r"(.*?)" + "\\" + delimiter_strs[2]
 
-# Iterating through the json
-# list
-for i in data['Scenario']:
-    print(i)
+    scenario = key['Scenario']
+    print(scenario)
+    print(re.findall(my_regex, scenario))
+
 
 # Closing file
 f.close()
