1f3316c846533d2403ee58d530594a2ff63b5529
Emilie Echevin <90863470+Emimint@users.noreply.github.com>
2023-03-31 08:11:08 -0400

    update formatter.py

--- formatter.py
+++ formatter.py
@@ -1,19 +1,12 @@
 import json
 import re
+from string import printable
 
-path_to_fileA = "scenarios.json"
-
-# Opening JSON file
-f = open(path_to_fileA)
-
-# returns JSON object as
-# a dictionary
-data = json.load(f)
+path_to_json = "scenarios.json"
+path_to_output = "all_scenarios.json"
 
 
-# print(data)
-# exit(0)
-
+# functions for string treatments:
 def camelcase_format(input_word):
     word_split = "".join(re.split(r"[;_' ]", input_word.title()))
     return word_split[0].lower() + word_split[1:]
@@ -29,26 +22,28 @@ def description_format(input_word):
     return word_split[0].upper() + word_split[1:]
 
 
-for key in data:
-    delimiter_strs = key['Delimiter']
-    my_regex = "\\" + delimiter_strs[0] + r"(.*?)" + "\\" + delimiter_strs[2]
+with open(path_to_json, "r") as read_file:  # Opening JSON file
 
-    scenario = key['Scenario']
-    word_list = re.findall(my_regex, scenario)
+    data = json.load(read_file)  # returns JSON object as a dictionary
 
-    var_list = list(map(lambda x: camelcase_format(x), word_list))
+    for key in data:
+        delimiter_strs = key['Delimiter']
+        my_regex = "\\" + delimiter_strs[0] + r"(.*?)" + "\\" + delimiter_strs[2]
 
-    description_list = list(map(lambda x: description_format(x), word_list))
+        word_list = re.findall(my_regex, key['Scenario'])
 
-    my_Dict =dict(zip(var_list, description_list))
+        var_list = list(map(lambda x: camelcase_format(x), word_list))  # create list of variables' name
 
-    my_var_Object = {"Variables": my_Dict}
+        description_list = list(map(lambda x: description_format(x), word_list))  # create list of variables' value
 
-    key.update(my_var_Object)
+        my_Dict = dict(zip(var_list, description_list))  # join lists into a dictionary
 
-    print(json.dumps(key))
+        my_var_Object = {"Variables": my_Dict}
 
-# Closing file
-f.close()
+        key.update(my_var_Object)  # add the "Variables" object
 
-# print scenarios and words found:
+        del key['Delimiter']  # remove the "Delimiter" object that we don't need anymore
+
+        with open(path_to_output, "a") as write_file:  # write each dictionary (json object) to output file
+            json.dump(key, write_file, indent=4)
+            write_file.write(',')
