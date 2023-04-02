1df42a75e96a1dcccdeaab90d00b987b5f9390c5
Mikey Nichols <mnix@journeytocode.io>
2023-04-02 00:21:23 -0400

    creates a file for init function

--- src/views/init.js
+++ src/views/init.js
@@ -6,8 +6,13 @@ import controller from "../controller/";
 const homeButton = document.querySelector('h1')
 
 const init = () => {
-
-  
+    // clearAnchor()
+    // anchor.innerHTML = `
+    // <section>
+    // <h2>Welcome</h2>
+    // <p>Fill in the blanks and be the funniest person in the Room!</p>
+    // <button>&gt; GO MAD</button>
+    // </section>`;
   document.querySelector("button").onclick = () =>
     renderScenarios(controller());
 };
