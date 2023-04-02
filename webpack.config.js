9b9c5ced1fc1e61bc34a59bf31c466fc7d04b473
Mikey Nichols <mnix@journeytocode.io>
2023-04-01 21:46:45 -0400

    updates webpack config to accept sass files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    mode: "development",
+    entry: path.resolve(__dirname, './src/app.js'),
+    devtool: "eval-source-map",
+    module: {
+        rules: [
+          {
+             test: /\.(js|jsx)$/,
+             exclude: /node_modules/,
+             use: ['babel-loader']
+          },
+          {
+             test: /\.(s(a|c)ss)$/,
+             use: ['style-loader','css-loader', 'sass-loader']
+          }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'main.js',
+    },
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'),
+        hot: true
+    },
 };
