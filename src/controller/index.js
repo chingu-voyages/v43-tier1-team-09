dbed8f8483c570ed7d4e81a87c22ebe80f02b739
Mikey Nichols <mnix@journeytocode.io>
2023-04-05 08:49:32 -0400

    imports renderInputPage into controller and calls it instead of console.log

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,11 +1,12 @@
 import json from '../model/stories.json' assert {type: 'json'};
+import renderInputPage from '../views/renderInputPage';
 
 // function that collect data on json file
 function collectScenarios() {
     return {
       json,
       renderInputs: (json, e) => {
-        return console.log(json[e].Scenario_title, json[e].Variables, e);
+        return renderInputPage(json[e].Scenario_title, json[e].Variables, e);
       },
     };
   }
