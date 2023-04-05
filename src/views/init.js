5c9af8d88e3d559e655cb15912bd76f64c094d26
Mikey Nichols <mnix@journeytocode.io>
2023-04-05 17:51:06 -0400

    renders header and footer on initialize

--- src/views/init.js
+++ src/views/init.js
@@ -1,9 +1,13 @@
+import '../styles/components/init.scss'
 // import clearAnchor, { anchor } from "./clearAnchor"; 
-
 import renderScenarios from "./renderScenarios";
 import controller from "../controller/";
+import renderHeader from './renderHeader';
+import renderFooter from './renderFooter';
 
 const init = () => {
+// render the header on screen
+renderHeader()
 // run the clear function
 
 // create a section element
@@ -15,7 +19,8 @@ const init = () => {
 // select the recently created button and create onclick event to call the render function and pass in the arguments required to work
   document.querySelector("button").onclick = () =>
     renderScenarios(controller());
+// render the footer on screen
+renderFooter()
 };
-
 
 export default init;
