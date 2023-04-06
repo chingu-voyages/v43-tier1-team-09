473ce562c3888091bf2c3724da2e6f0ef4a1fad6
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-04-06 19:34:47 -0300

    write a init function

--- src/views/init.js
+++ src/views/init.js
@@ -1,21 +1,22 @@
-// import clearAnchor, { anchor } from "./clearAnchor"; 
-
+// import clearAnchor, { anchor } from "./clearAnchor";
+import clearAnchor, { anchor } from "./clearAnchor";
 import renderScenarios from "./renderScenarios";
-import controller from "../controller/";
+import controller from "../controller/index.js";
 
 const init = () => {
-// run the clear function
-
-// create a section element
-
-// fill in the section element with the HTML from index.html
-
-// append the section element to the anchor container
-
-// select the recently created button and create onclick event to call the render function and pass in the arguments required to work
+  // run the clear function
+  clearAnchor();
+  // create a section element
+  const section = document.createElement("section");
+  // fill in the section element with the HTML from index.html
+  section.innerHTML = `<h2>Welcome</h2>
+  <p>Fill in the blanks and be the funniest person in the Room!</p>
+  <button>&gt; GO MAD</button>`;
+  // append the section element to the anchor container
+  anchor.append(section);
+  // select the recently created button and create onclick event to call the render function and pass in the arguments required to work
   document.querySelector("button").onclick = () =>
     renderScenarios(controller());
 };
-
 
 export default init;
