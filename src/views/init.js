029ec4a9d02d9276109129cc551f71ea4977e61b
Gabriela <118011195+PaulaR-05@users.noreply.github.com>
2023-04-07 13:47:30 -0300

    Write init function, to initialize the app

--- src/views/init.js
+++ src/views/init.js
@@ -1,26 +1,29 @@
-import '../styles/components/init.scss'
-// import clearAnchor, { anchor } from "./clearAnchor"; 
+import "../styles/components/init.scss";
+// import clearAnchor, { anchor } from "./clearAnchor";
+import clearAnchor, { anchor } from "./clearAnchor";
 import renderScenarios from "./renderScenarios";
-import controller from "../controller/";
-import renderHeader from './renderHeader';
-import renderFooter from './renderFooter';
+import controller from "../controller/index.js";
+import renderHeader from "./renderHeader";
+import renderFooter from "./renderFooter";
 
 const init = () => {
-// render the header on screen
-renderHeader()
-// run the clear function
-
-// create a section element
-
-// fill in the section element with the HTML from index.html
-
-// append the section element to the anchor container
-
-// select the recently created button and create onclick event to call the render function and pass in the arguments required to work
+  // render the header on screen
+  renderHeader();
+  // run the clear function
+  clearAnchor();
+  // create a section element
+  const section = document.createElement("section");
+  // fill in the section element with the HTML from index.html
+  section.innerHTML = `<h2>Welcome</h2>
+  <p>Fill in the blanks and be the funniest person in the Room!</p>
+  <button>&gt; GO MAD</button>`;
+  // append the section element to the anchor container
+  anchor.append(section);
+  // select the recently created button and create onclick event to call the render function and pass in the arguments required to work
   document.querySelector("button").onclick = () =>
     renderScenarios(controller());
-// render the footer on screen
-renderFooter()
+  // render the footer on screen
+  renderFooter();
 };
 
 export default init;
