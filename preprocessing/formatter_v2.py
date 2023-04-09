8a698e93b4842d2aa39529f2d6b970d186f3bcfb
Emimint <90863470+Emimint@users.noreply.github.com>
2023-04-09 11:01:51 -0400

    Create formatter_v2.py

---
+++ preprocessing/formatter_v2.py
@@ -0,0 +1,38 @@
+import json
+import re
+from string import printable
+
+path_to_json = "input_scenarios.json"  # table with json object
+path_to_output = "scenarios.json"
+
+
+with open(path_to_json, "r") as read_file:  # Opening JSON file
+
+    data = json.load(read_file)  # returns JSON object as a dictionary
+
+    for key in data:
+        my_regex = "{{" + r"(.*?)" + "}}"
+        var_list = re.findall(my_regex, key['Scenario'])
+        description_list = []
+
+        for var in var_list:
+            description = re.findall('[a-zA-Z][^A-Z]*', var)
+            description_str = " ".join(
+                list(map(lambda x: x.lower(), description)))
+            description_str = description_str[0].upper() + description_str[1:]
+            description_list.append(description_str)
+
+        for f, b in zip(var_list, description_list):
+            print(f, b)
+
+        # join lists into a dictionary
+        my_Dict = dict(zip(var_list, description_list))
+
+        my_var_Object = {"Variables": my_Dict}
+
+        key.update(my_var_Object)  # add the "Variables" object
+
+        # write each dictionary (json object) to output file
+        with open(path_to_output, "a") as write_file:
+            json.dump(key, write_file, indent=4)
+            write_file.write(',')
