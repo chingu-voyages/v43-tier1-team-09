41ca3a43b8af409d663e4a2928c89957407c9e7b
Mikey Nichols <mnix@journeytocode.io>
2023-04-09 23:55:44 -0400

    create logic to change the design of the header dynamically on page change

--- src/views/renderScenarios.js
+++ src/views/renderScenarios.js
@@ -1,13 +1,24 @@
+// imports other views from our project
 import clearAnchor, { anchor } from "./clearAnchor";
+import renderHeader from './renderHeader';
+import renderFooter from './renderFooter';
 // function that render scenarios on screen and send the variables to input Page
 async function renderScenarios(data) {
+  // when we render scenarios, it adds a class to the rocket ship, which causes it to 'blast off'
   document.querySelector('.rocket').classList.add('blast-off')
+  // calls the renderHeader function, passing in "SelectPage" as the argument, which tells the header we are on this page
+  renderHeader('SelectPage');
+  // defines our scenarios from the data being passed into this function
   const scenarios = data.json;
+  // creates DOM elements for our scenarios
   const section = document.createElement("section");
   const heading = document.createElement("h2");
   const ul = document.createElement("ul");
+  // maps over our scenarios to create individual items from each story
   scenarios.map((story) => {
+    // creates an html list item for each scenario
     const li = document.createElement("li");
+    // defines the inner html of each list item to have a div with an image and a link inside to the story
     li.innerHTML = `
       <div>
                 <img
@@ -18,18 +29,26 @@ async function renderScenarios(data) {
               </div>
         
       `;
+    // appends the list item to the unordered list we created
     ul.append(li);
   });
+  // Applies a class list for styling the unordered list
   ul.classList = "flex wrap no-list center";
+  // Defines the static text to be rendered on screen for the heading
   heading.innerText = "Please select one of the stories below to play:";
+  // defines a class of "game" on the section
   section.classList = "game";
+  // Runs the function to clear our anchor
   clearAnchor();
+  // stitches the DOM together
   section.prepend(heading);
   section.append(ul);
   anchor.append(section);
+  // Loops over all of the newly created list items and creates onclick functions for each of them which will call the next function in line passing in important variables as need be
   document
     .querySelectorAll(".game ul li")
     .forEach((e, i) => (e.onclick = () => data.renderInputs(scenarios, i)));
+    renderFooter("SelectPage")
 }
 
 export default renderScenarios;
