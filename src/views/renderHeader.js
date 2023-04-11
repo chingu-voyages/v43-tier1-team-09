a0ace1077d8f8570aeea7a5046b990988ae0e344
Mikey Nichols <mnix@journeytocode.io>
2023-04-11 00:27:43 -0400

    updates logic to change styles based upon the page the user is on

--- src/views/renderHeader.js
+++ src/views/renderHeader.js
@@ -19,7 +19,7 @@ const renderHeader = (page) => {
   document.body.prepend(header);
   const world = document.querySelector(".world"); // creates a variable for the world div that we just created
   // runs the init function when we click on the world container
-  //world.onclick = init; (Nazille's task)
+  world.onclick = init;
   switch (page) {
     case "SelectPage":
       // Story Select Screen Header
@@ -36,13 +36,18 @@ const renderHeader = (page) => {
       // immediately invokes an asynchronous function to remove the class that places it off the screen to the right.
       setTimeout(() => topRightDots.classList.remove("shift-right"), 1);
       break;
-    // case "InputPage":
-    //   console.log("Input Screen");
-    //   break;
-    // case "StoryPage":
-    //   console.log("Rendered Story Screen");
-    //   break;
-    /* The above lines are commented out as at the moment they are not being utilized for rendering anything on screen, however they can be utilized to render different header states */
+    case "InputPage":
+      // Story Input Page Header
+      topLeftDots.classList = "shift-up dots__top-left"; // defines the dots on the left to render off the screen to the top initially
+      document.body.prepend(topLeftDots); // appends the left dots onto the screen
+      setTimeout(() => topLeftDots.classList.remove("shift-up"), 1); // immediately removes the class that places the dots on the left off screen, which places them on screen
+      break;
+    case "StoryPage":
+      // Adds the class of shift-up to dots on screen causing them to shift off screen.
+      topLeftDots.classList.add("shift-up");
+      // After 300ms we remove the element from the DOM
+      setTimeout(() => topLeftDots.remove(), 300);
+      break;
     default:
       // Landing Page Header
       // Adds a class to the dots on the right that shifts them off screen to the right
