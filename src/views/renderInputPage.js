e5b1c456aaa00c05564217c3f45f70642b7fda1b
Mikey Nichols <mnix@journeytocode.io>
2023-04-12 01:26:48 -0400

    moves renderHeader above form, sets data-attribute on input along with some additional comments

--- src/views/renderInputPage.js
+++ src/views/renderInputPage.js
@@ -16,6 +16,9 @@ const renderInputPage = (title, variables, index) => {
   const h3 = document.createElement("h3");
   h2.innerHTML = title; // dynamically changes the title
   h3.innerText = "Fill in the blank fields below.";
+  // calls the renderHeader function, passing in "SelectPage" as the argument, which tells the header we are on this page
+  renderHeader("InputPage");
+  // appends the headings to the form and section
   section.append(h2);
   form.append(h3);
   // Maps over the keys array to render divs with labels and placeholders for form inputs
@@ -25,26 +28,33 @@ const renderInputPage = (title, variables, index) => {
           <label for="${key}">${placeholders[i]}: </label>
           <input type="text" name="${key}" id="${key}" required />
         `;
-    input.setAttribute('data-completed', false)
+    input.setAttribute("data-completed", false);
+    const button = document.createElement("button");
+    button.innerText = `> ${placeholders[i]}`;
     form.append(input);
   });
-  // calls the renderHeader function, passing in "SelectPage" as the argument, which tells the header we are on this page
-  renderHeader("InputPage");
+
   // event listener must be inside of this function as this is where the form is rendered.
   form.addEventListener("submit", (e) => {
     e.preventDefault(); // stops page from following default protocol to process the form
     const formResponses = [...new FormData(e.target).entries()].map(
       (data) => data[1]
     ); // converts responses from form into an array to be passed into the function to collect scenarios
-    if (pegarDifferenca(formResponses.map(response => response.toLowerCase()))) // converts each response to a lowercase object to check if uppercase curse word;
+    console.log(formResponses);
+    if (
+      pegarDifferenca(formResponses.map((response) => response.toLowerCase()))
+    )
+      // converts each response to a lowercase object to check if uppercase curse word;
       // checks the responses given against a badword filter
       controller().renderStory(index, formResponses, keys); // render the story since it passed the check
   });
   // creates a button at the bottom of the form
   const button = document.createElement("button");
   button.innerText = `> Go Mad!`;
+  // renders the button on screen
   form.append(button);
   clearAnchor();
+  // appends the section and form to the anchor
   section.append(form);
   anchor.append(section);
   // calls the renderFooter function passing in this page
