7e1d0485beb256b6553c5750b7d60dd548a253ff
Mikey Nichols <mnix@journeytocode.io>
2023-04-15 07:51:24 -0400

    adds logic to dynamically generate when we update devs.json our devs page

--- src/views/team/renderDev.js
+++ src/views/team/renderDev.js
@@ -1,46 +1,27 @@
-import { getGitHubUser, getDevs } from "../../controller";
+import { getGitHubUser } from "../../controller";
 
-const renderDev = async (username) => {
-  const user = await getGitHubUser(username);
-  const { bio, name, location } = user;
+const renderDev = async (developer) => {
+  const user = await getGitHubUser(developer.github);
+  let { bio, name, location } = user;
   const teamAnchor = document.querySelector(".container__card");
   const dev = document.createElement("div");
-  console.log(username);
-  if (name == null || name == undefined)
-    switch (username) {
-      case "Nazile-Tag":
-        dev.classList = "container__card--information";
-        dev.innerHTML = `
-        <a href="https://github.com/Nazile-Tag" target="_blank">
-        <img src='./assets/images/Nazile-Tag.png' alt='Nazile-Tag' />
-        <h2>Nazile</h2>
-        <p class="title">Developer</p>
-        `;
-        teamAnchor.append(dev);
-        return;
-      default:
-        console.error("What's going on?", username);
-    }
+  if (name == null || name == undefined) name = developer.name;
+  console.log(name);
 
   dev.classList = "container__card--information";
   dev.innerHTML = `
-      <a href="https://github.com/${username}" target="_blank">
-      <img src='./assets/images/${username}.png' alt='${name}' />
+      <a href="https://github.com/${developer.github}" target="_blank">
+      <img src='./assets/images/${developer.github}.png' alt='${name}' />
       <h2>${name}</h2>
-      <p class="title">Developer</p>
+      <p class="title">${developer.role}</p>
       `;
   // each dev has a potential to show their perpspectives or reflect on the project
-  const aboutContainer = document.getElementById('about-container');
+  const aboutContainer = document.getElementById("about-container");
   dev.onmouseover = () => {
-    switch (username) {
-      case "mnichols08":
-        aboutContainer.innerHTML= `<p class="about-container">${getDevs()[3].story}</p>`;
-        break;
-      default:
-        break;
-    }
+    if (developer.story)
+      aboutContainer.innerHTML = `<p class="about-container">${developer.story}</p>`;
   };
-  dev.onmouseout = () => aboutContainer.innerHTML = '';
+  dev.onmouseout = () => (aboutContainer.innerHTML = "");
   teamAnchor.append(dev);
 };
 
