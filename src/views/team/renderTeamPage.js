d6850b3d77ddc02c297a6428c2d065d388504a3d
Mikey Nichols <mnix@journeytocode.io>
2023-04-15 00:07:50 -0400

    creates a container to render experience to

--- src/views/team/renderTeamPage.js
+++ src/views/team/renderTeamPage.js
@@ -18,15 +18,17 @@ const renderTeamPage = () => {
   renderDev("Nazile-Tag");
   teamAnchor.append(section);
   anchor.append(teamAnchor);
+  const aboutContainer = document.createElement("p");
+  aboutContainer.id = "about-container";
   const feedbackContainer = document.createElement("p");
   feedbackContainer.innerHTML = `<a
   href="https://docs.google.com/forms/d/e/1FAIpQLSd91zh13dCmv4GNsG7ndVoY4njof7NvHQ3LoMrXabnkXylihg/viewform?usp=sf_link"
   target="_blank">
   Please report any &#128027;<a id="enableBugs">bugs</a>&#128027; or provide
   feedback 😀`;
+  anchor.append(aboutContainer);
   anchor.append(feedbackContainer);
   document.getElementById("enableBugs").onclick = () => renderBugs();
-  // document.getElementById("mikey").onclick = () => renderDev("mnichols08");
   renderFooter("TeamPage");
 };
 
