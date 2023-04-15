131ce7abfcca5f969a43edf96c303e401f8908b4
Mikey Nichols <mnix@journeytocode.io>
2023-04-14 23:55:23 -0400

    calls renderDev to render the dev page dynamically

--- src/views/team/renderTeamPage.js
+++ src/views/team/renderTeamPage.js
@@ -2,56 +2,31 @@ import clearAnchor, { anchor } from "../../utils/clearAnchor";
 import renderHeader from "../renderHeader";
 import renderFooter from "../renderFooter";
 import renderBugs from "./renderBugs";
-import renderMikey from './renderMikey';
+import renderDev from "./renderDev";
 
 const renderTeamPage = () => {
   clearAnchor();
   renderHeader("TeamPage");
   const section = document.createElement("section");
   section.classList = "container fade-in";
-  section.innerHTML = `
-      <div class="container__card">
-      <div class="container__card--information">
-        <a href="https://github.com/Emimint" target="_blank"><img src="./assets/images/emilie.png" alt="Emilie" />
-          <h2>Emilie Echevin</h2>
-        </a>
-        <p class="title">Developer</p>
-      </div>
-
-      <div class="container__card--information">
-        <a href="https://github.com/emilio12345" target="_blank"><img src="./assets/images/mili.png" alt="Emilio" />
-          <h2>Emilio Rivera</h2>
-        </a>
-        <p class="title">Developer</p>
-      </div>
-
-      <div class="container__card--information">
-        <a href="https://github.com/PaulaR-05" target="_blank"><img src="./assets/images/gabriela.png" alt="gabriela" />
-          <h2>Gabriela de Paula</h2>
-        </a>
-        <p class="title">Developer</p>
-      </div>
-
-      <div class="container__card--information">
-        <a id="mikey" target="_blank"><img src="./assets/images/mikey.png" alt="mikey" />
-          <h2>Mikey Nichols</h2>
-        </a>
-        <p class="title">Developer</p>
-      </div>
-
-      <div class="container__card--information">
-        <a href="https://github.com/Nazile-Tag" target="_blank"><img src="./assets/images/naz.png" alt="Nazile" />
-          <h2>Nazile</h2>
-        </a>
-        <p class="title">Developer</p>
-      </div>
-
-    </div>
-    <a href="https://docs.google.com/forms/d/e/1FAIpQLSd91zh13dCmv4GNsG7ndVoY4njof7NvHQ3LoMrXabnkXylihg/viewform?usp=sf_link" target="_blank">Please report any &#128027;<a id="enableBugs">bugs</a>&#128027; or provide feedback 😀</a>
-   `;
-  anchor.append(section);
+  const teamAnchor = document.createElement("div");
+  teamAnchor.classList = "container__card";
+  renderDev("Emimint");
+  renderDev("emilio12345");
+  renderDev("PaulaR-05");
+  renderDev("mnichols08");
+  renderDev("Nazile-Tag");
+  teamAnchor.append(section);
+  anchor.append(teamAnchor);
+  const feedbackContainer = document.createElement("p");
+  feedbackContainer.innerHTML = `<a
+  href="https://docs.google.com/forms/d/e/1FAIpQLSd91zh13dCmv4GNsG7ndVoY4njof7NvHQ3LoMrXabnkXylihg/viewform?usp=sf_link"
+  target="_blank">
+  Please report any &#128027;<a id="enableBugs">bugs</a>&#128027; or provide
+  feedback 😀`;
+  anchor.append(feedbackContainer);
   document.getElementById("enableBugs").onclick = () => renderBugs();
-  document.getElementById('mikey').onclick = () => renderMikey();
+  // document.getElementById("mikey").onclick = () => renderDev("mnichols08");
   renderFooter("TeamPage");
 };
 
