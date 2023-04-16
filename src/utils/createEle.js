5e9920a74872d1d848fe3eaef379b29a8f136e77
Mikey Nichols <mnix@journeytocode.io>
2023-04-16 14:04:38 +0000

    adds comments to explain function

--- src/utils/createEle.js
+++ src/utils/createEle.js
@@ -1,8 +1,8 @@
 // function to create an element, set the innerHTML to content, and append it to a root element
 const createEle = (ele, content, root) => {
-    let container = document.createElement(ele);
-    container.innerHTML = content;
-    root.append(container);
+    let container = document.createElement(ele); // creates an element from the ele variable name 
+    container.innerHTML = content; // applies the innerHTML of the container to the content provided to function
+    root.append(container); // appends the container to the root container passed into function
     return container;
   };
   
