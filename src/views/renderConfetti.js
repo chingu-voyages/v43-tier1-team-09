7d25f678b5737b083802f92e47549d1f66904f86
Mikey Nichols <mnix@journeytocode.io>
2023-04-16 15:56:00 +0000

    refactors confetti to be a for loop instead of for each and also imports and leverages createEle

--- src/views/renderConfetti.js
+++ src/views/renderConfetti.js
@@ -1,20 +1,18 @@
+import createEle from '../utils/createEle';
 import { visuals } from '../utils/clearAnchor';
 
 const renderConfetti = () => {
   // defines a mutable variable
   let confettiDiv;
-  if (document.getElementById("confetti")) return;
+  if (document.getElementById("confetti")) return; // returns immediately if we already have a confetti container on screen
   // define a confetti div container
-  else confettiDiv = document.createElement("div");
-  confettiDiv.id = "confetti";
-  // defines an array of numbers to create confetti from
-  const arr = [1, 2, 3, 4, 5];
-  // For each confetti that we just created
-  arr.forEach((e) => {
-    // we create a div element to render on screen
-    const confetti = document.createElement("div");
+  else confettiDiv = createEle('div', '', visuals); // creates a div to render confetti within and appends to the visuals container
+  confettiDiv.id = "confetti"; // applies an id of confetti to our confetti container
+
+  for (let i = 1; i <= 5; i++) {
+    const confetti = createEle('div', '', confettiDiv) // creates a confetti div (up to 5)
     // for the classList we add in the number
-    confetti.classList.add(`confetti-${e}`);
+    confetti.classList.add(`confetti-${i}`); // passes in our 
     // Generate a random time to run animation
     const randomTime = `${Math.floor(Math.random() * 3) + 1}s infinite`;
     // I have 3 animations in my SCSS so I want to randomly pick one, this is an array of those animations
@@ -23,9 +21,7 @@ const renderConfetti = () => {
     confetti.style.animation = `${
       animations[Math.floor(Math.random() * animations.length)]
     } ${randomTime}`;
-    confettiDiv.append(confetti);
-  });
-  visuals.prepend(confettiDiv);
+  };
 };
 
 export default renderConfetti;
