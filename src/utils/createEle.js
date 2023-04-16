fcb10430705f21b0ec287265ae69a2603d12b209
Mikey Nichols <mnix@journeytocode.io>
2023-04-16 16:41:02 +0000

    creates logic to check if the element rendered is a header or not

--- src/utils/createEle.js
+++ src/utils/createEle.js
@@ -2,8 +2,9 @@
 const createEle = (ele, content, root) => {
     let container = document.createElement(ele); // creates an element from the ele variable name 
     container.innerHTML = content; // applies the innerHTML of the container to the content provided to function
-    root.append(container); // appends the container to the root container passed into function
-    return container;
+    if (ele == 'header') root.prepend(container); // checks if we are rending a header element and prepends if so
+    else root.append(container); // appends the container to the root container passed into function otherwise
+    return container; // returns the value of the container that we created so functions can set the values later
   };
   
   export default createEle;
