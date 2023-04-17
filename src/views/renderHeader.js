63ec6b17f18eea59c36d3b7b40eb6c60816684e7
Mikey Nichols <mnix@journeytocode.io>
2023-04-16 22:41:28 -0400

    create static values for html being rendered

--- src/views/renderHeader.js
+++ src/views/renderHeader.js
@@ -2,6 +2,18 @@ import createEle from "../utils/createEle"; // imports createEle function
 import init from "./init"; // pulls in the initiliaze application function to be called on demand
 import { visuals } from "../utils/clearAnchor";
 const renderHeader = (page) => {
+  // define static header html
+  const headerHTML = `<div class="world"></div><h1 class="heading-primary">Mad Libs</h1>`;
+  const devHTML = `
+  <div class="right shift-right">
+  <div class="world"></div>
+  <h1 class="heading-primary">Mad Libs</h1>
+  </div>
+  <div class="header">
+  <h1>Meet Our <span>Devs</span></h1>
+  <p></p>
+  </div>
+  `; // defines devHTML
   // defines dots div containers or creates them as needed
   let topLeftDots =
     document.querySelector(".dots__top-left") || document.createElement("div");
@@ -9,18 +21,11 @@ const renderHeader = (page) => {
     document.querySelector(".dots__top-right") || document.createElement("div");
   // checks if we have a header or not and creates it otherwise
   let header;
-  if (document.querySelector("header"))
+  if (document.querySelector("header") !== null)
     header = document.querySelector("header");
-  else
-    header = createEle(
-      "header",
-      `
-  <div class="world"></div>
-  <h1 class="heading-primary">Mad Libs</h1>`,
-      document.body
-    ); // creates a header element and appends to the body
+  else header = createEle('header', headerHTML, document.body); // creates a header element fills with default static html and prepends it to the body
   // checks whether this function has a page defined then immediately invokes asyncynous code to add the class
-  if (page) setTimeout(() => header.classList = `fade-in ${page}`, 1);
+  if (page) setTimeout(() => (header.classList = `fade-in ${page}`), 1);  
   const world = document.querySelector(".world"); // creates a variable for the world div that we just created
   // runs the init function when we click on the world container
   world.onclick = () => init();
@@ -31,8 +36,6 @@ const renderHeader = (page) => {
       topLeftDots.classList.add("shift-left");
       // After 300ms we remove the element from the DOM
       setTimeout(() => topLeftDots.remove(), 300);
-      // prepends the header to the document body
-      document.body.prepend(header);
       // defines the dots on the top right to initially be off the screen
       topRightDots.classList = "shift-right dots__top-right";
       // prepends the dots on the top right to the body
@@ -53,20 +56,8 @@ const renderHeader = (page) => {
       setTimeout(() => topLeftDots.remove(), 300);
       break;
     case "TeamPage":
-      // renders the html from Gabriela's design file and adds the logo from before but inside of a div
-      header.innerHTML = `
-      <div class="right shift-right">
-        <div class="world"></div>
-        <h1 class="heading-primary">Mad Libs</h1>
-      </div>
-      <div class="header">
-      
-      <h1>Meet Our <span>Devs</span></h1>
-      <p></p>
-      </div>
-      `;
-      header.classList = ""; // removes the class from header
-      header.classList.add("fade-in"); // adds class of fade-in, causing the header to fade in when this page loads
+      //renders the html from Gabriela's design file and adds the logo from before but inside of a div
+      header.innerHTML = devHTML; // sets innerHTML of header to be the devHTML defined above
       const rightHeader = document.querySelector(".right"); // defines the right container we created above
       const rightWorld = document.querySelector(".world"); // defines the world that was created above
       rightWorld.onclick = () => init(); // defines a function to run init() when the user clicks on the world (returning home)
@@ -78,8 +69,7 @@ const renderHeader = (page) => {
       setTimeout(() => topLeftDots.classList.remove("shift-up"), 1); // immediatley invokes a function to remove the class hiding them, causing them to slide into view
       break;
     default:
-      header.innerHTML = `<div class="world"></div><h1 class="heading-primary">Mad Libs</h1>`;
-      header.classList = "fade-in";
+      header.innerHTML = headerHTML;     
       // Landing Page Header
       // Adds a class to the dots on the right that shifts them off screen to the right
       topRightDots.classList.add("shift-right");
@@ -90,6 +80,7 @@ const renderHeader = (page) => {
       visuals.prepend(topLeftDots); // appends the left dots onto the screen
       setTimeout(() => topLeftDots.classList.remove("shift-left"), 1); // immediately removes the class that places the dots on the left off screen, which places them on screen
   }
+  
 };
 
 export default renderHeader;
